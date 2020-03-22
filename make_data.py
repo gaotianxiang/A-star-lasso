9098f34333138c4e7f62a614dcb56d77055c675c
Tianxiang Gao <gtx9726@gmail.com>
2020-03-22 10:44:08 -0400

    make data file clean

--- make_data.py
+++ make_data.py
@@ -1,14 +1,7 @@
-#
-# netid = 16; %for insurance network
-# nsets = 10; %10 repetitions
-# lowerbd = 0.5;  %lower bound on beta values
-# upperbd = 1; %upper bound on beta values
-# genSimuData(netid,nsets,lowerbd,upperbd)
-
 from gen_simu_data import gen_simu_data
 
 net_id = 15
-num_sets = 1
+num_sets = 10
 lower_bd = 0.5
 upper_bd = 1
 num_samples = 2500
