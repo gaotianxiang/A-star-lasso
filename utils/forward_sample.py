b560c6b93392500c3544e57423f4f58e6f1a499b
Tianxiang Gao <gtx9726@gmail.com>
2020-03-22 10:46:27 -0400

    forward sample function clean

--- utils/forward_sample.py
+++ utils/forward_sample.py
@@ -2,13 +2,13 @@ import numpy as np
 
 
 def forward_sample(adj_matrix, top_order, num_samples, q, lower_bd, upper_bd):
-    print(top_order)
     data = np.zeros((num_samples, q))
     sigma = 1
-    s = np.random.binomial(1, 0.5, (q, 1))
+    s = np.random.binomial(1, 0.5, q)
     s[s == 0] = -1
-    beta_y = np.random.uniform(lower_bd, upper_bd, (q, 1))
+    beta_y = np.random.uniform(lower_bd, upper_bd, q)
     beta_y *= s
+    beta_true = np.zeros((q, q))
 
     for j in range(len(top_order)):
         curr = top_order[j]
@@ -16,7 +16,8 @@ def forward_sample(adj_matrix, top_order, num_samples, q, lower_bd, upper_bd):
         if len(pay) == 0:
             data[:, curr] = np.random.normal(0, 1, num_samples)
         else:
-            m = np.sum(data[:, pay] * beta_y[pay], axis=1)
+            m = np.sum(data[:, pay] * beta_y[pay], axis=1).squeeze()
             data[:, curr] = np.random.normal(m, sigma)
+            beta_true[pay, j] = beta_y[pay]
 
-    return data, beta_y
+    return data, beta_y, beta_true
