26f29576352e175b7bc65ce26e7ed1d801575cfd
Tianxiang Gao <gtx9726@gmail.com>
2020-03-23 12:50:06 -0400

    update gen simu date universal interface

--- utils/gen_simu_data.py
+++ utils/gen_simu_data.py
@@ -5,21 +5,21 @@ import pickle
 from utils import forward_sample
 from utils import plot_network
 
-networks = ['tree', 'inversetree', 'factors', 'alarm', 'barley', 'carpo', 'chain',
-            'hailfinder', 'insurance', 'mildew', 'water', 'vstructure', 'treebranch',
-            'inversetreebranch', 'skinnytree', 'asia', 'dsep', 'bowling', 'funnel',
-            'insurancesmall', 'alarm300', 'walrus', 'shallow21', 'chain20', 'rain',
-            'cloud', 'galaxy', 'hailfinder300']
+networks_name = ['tree', 'inversetree', 'factors', 'alarm', 'barley', 'carpo', 'chain',
+                 'hailfinder', 'insurance', 'mildew', 'water', 'vstructure', 'treebranch',
+                 'inversetreebranch', 'skinnytree', 'asia', 'dsep', 'bowling', 'funnel',
+                 'insurancesmall', 'alarm300', 'walrus', 'shallow21', 'chain20', 'rain',
+                 'cloud', 'galaxy', 'hailfinder300']
 
 
 def gen_simu_data(net_id, num_sets, lower_bd, upper_bd, num_samples):
-    name = networks[net_id]
-    func_name = 'get_dag_{}'.format(name)
-    # adj_matrix, node_names, top_order = getattr(networks, func_name)
-    adj_matrix, node_names, top_order = networks.get_dag_asia()
+    name = networks_name[net_id]
+    network_generator = networks.Network()
+    get_dag = network_generator.get_dag(name)
+    adj_matrix, node_names, top_order = get_dag()
 
     q = len(node_names)
-    dir_name = '../simdata'
+    dir_name = './simdata'
     os.makedirs(dir_name, exist_ok=True)
     plot_network(adj_matrix, node_names, dir_name, name)
 
@@ -32,9 +32,9 @@ def gen_simu_data(net_id, num_sets, lower_bd, upper_bd, num_samples):
         file_name = os.path.join(dir_name, name, '{}_lb{}_ub{}_set{}.pkl'.format(name, lower_bd * 10,
                                                                                  upper_bd * 10, i))
         os.makedirs(os.path.dirname(file_name), exist_ok=True)
-        data, beta_true, beta_matrix = forward_sample(adj_matrix, top_order, num_samples, q, lower_bd, upper_bd)
+        data, beta_matrix = forward_sample(adj_matrix, top_order, num_samples, q, lower_bd, upper_bd)
 
-        with open(file_name, 'wb+') as file:
+        with open(file_name, 'wb') as file:
             content = {
                 'Y': data,
                 'Ayy': adj_matrix,
@@ -42,7 +42,6 @@ def gen_simu_data(net_id, num_sets, lower_bd, upper_bd, num_samples):
                 'name': name,
                 'lowerbd': lower_bd,
                 'upper': upper_bd,
-                'betature': beta_true,
                 'betamatrix': beta_matrix
             }
             pickle.dump(content, file)
